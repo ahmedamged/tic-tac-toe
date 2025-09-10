ed5df87dc960929fc7fc276a8665edd9c9f79450
Ahmed Amged <ahmedanged@live.com>
2025-09-10 15:17:04 +0300

    Added jsx and styles for Tic Tac Toe game

---
+++ src/components/TicTacToe.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./TicTacToe.css";
+
+export const TicTacToe = () => {
+  return (
+    <div className="container">
+      <h1 className="title">
+        Tic Tac Toe In <span>React</span>
+      </h1>
+      <div className="board">
+        <div className="row1">
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+        </div>
+        <div className="row2">
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+        </div>
+        <div className="row3">
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+        </div>
+      </div>
+      <button className="reset">Reset</button>
+    </div>
+  );
+};
